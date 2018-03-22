ea94bac6f88cebdaf65c9f03f474b5101484db3b
Edward Faulkner <ef@alum.mit.edu>
2018-03-22 01:48:18 -0400

    using module unification layout

--- src/resolver.js
+++ src/resolver.js
@@ -1,3 +1,14 @@
-import Resolver from 'ember-resolver';
+import Resolver from 'ember-resolver/resolvers/fallback';
+import buildResolverConfig from 'ember-resolver/ember-config';
+import config from '../config/environment';
+import { merge } from '@ember/polyfills';
 
-export default Resolver;
+let moduleConfig = buildResolverConfig(config.modulePrefix);
+merge(moduleConfig.types, {
+  config: { definitiveCollection: 'main' },
+});
+moduleConfig.collections.main.types.push('config');
+
+export default Resolver.extend({
+  config: moduleConfig
+});
